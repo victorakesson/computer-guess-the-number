bb170f819401c978992dff74749c0b7b21e74cb9
victorakesson <65647015+victorakesson@users.noreply.github.com>
2020-11-16 15:27:20 +0100

    Alla uppgift förutom fördjupning

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const MAX_NUMBER = 100
 const MIN_NUMBER = 0
 
-let minGuess = MIN_NUMBER
-let maxGuess = MAX_NUMBER
+let NewMaxNumber = 100 
+let NewMinNumber = 0 
 
-let guess = 50 
+let guess  
 
 document.getElementById('too-high-btn').addEventListener('click', function () {
     // Vad ska hända när man tryckt too high?
-    guess--
+    NewMaxNumber = guess
+    guess = LowMiddleNumber(NewMinNumber,guess)
     setMessage(`Is it ${guess}?`) 
     
 
@@ -16,14 +17,15 @@ document.getElementById('too-high-btn').addEventListener('click', function () {
 
 document.getElementById('too-low-btn').addEventListener('click', function () {
     // Vad ska hända när man tryckt too low?
-    guess++
+    NewMinNumber = guess
+    guess = HighMiddleNumber(NewMaxNumber, guess)
     setMessage(`Is it ${guess}?`) 
     
 })
 
 document.getElementById('is-correct-btn').addEventListener('click', function () {
 
-    setMessage("WOOHOO!") 
+    setMessage("YAY!") 
 })
 
 
@@ -36,12 +38,35 @@ function start() {
     document.getElementById('before-start').style.display = 'none'
     document.querySelector('main').style.display = 'block'
 
-    guess = (minGuess + maxGuess) / 2           /*Math.floor(Math.random() * 101)*/
-
+    makeGuess() 
     setMessage(`Is it ${guess}?`)
 }
+
+globalThis.setMessage = setMessage; 
 
 document
     .getElementById('start-btn')
     .addEventListener('click', start)
+
+function makeGuess() {
+    guess = Math.floor(Math.random() * 101)
+}
+
+function HighMiddleNumber(max, highGuess){
+    console.log("high")
+    highGuess = (max + guess)/2
+    console.log(guess)
+    return Math.round(highGuess)
+}
+
+function LowMiddleNumber(min, lowGuess){
+    console.log("low")
+    lowGuess = (min + guess)/2
+    console.log(guess)
+    return Math.round(lowGuess)
+}
+
+function setCounter() {
+    
+}
 
